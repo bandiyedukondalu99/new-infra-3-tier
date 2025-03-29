f146c1f6cca47e9dff89c256cfe2e2dd7e0ebb65
bandiyedukondalu99 <bandiyedukondalu99@gmail.com>
2025-03-29 06:14:34 -0700

    s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraformkonda"
+  bucket = "swiggy-bucket-terraformsidhiwqwkj"
 
   # Enable versioning
   versioning {
